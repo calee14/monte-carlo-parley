bdcf07f1328361284c26fcf8edddd426a9cb6846
Cappillen Lee <cappillen@ucsb.edu>
2025-04-11 03:23:35 -0700

    clean up simulation

--- app/views/simulation.py
+++ app/views/simulation.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, json, request, redirect, url_for
 
-simulation_bp = Blueprint("simulation", __name__, url_prefix="/simulation/")
+simulation_bp = Blueprint("simulation", __name__, url_prefix="/simulation")
 
 
 @simulation_bp.route("/")
